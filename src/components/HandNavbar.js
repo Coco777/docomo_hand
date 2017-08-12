2f5108538ff68df41ad38d7399587f9e9c6d1f63
Koki Sato <s.4115.koki@gmail.com>
2017-08-13 00:38:28 +0900

    Enable to use font-awesome in React

--- src/components/HandNavbar.js
+++ src/components/HandNavbar.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FlatButton from 'material-ui/FlatButton';
+import FontAwesome from 'react-fontawesome';
 
 const styles = {
   title: {
     cursor: 'pointer',
     fontWeight: 'bold',
   },
+  edit: {
+    fontSize: 16,
+  },
 };
 
 const HandNavbar = () => (
   <AppBar
     title={<span style={styles.title}>マイページ</span>}
-    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-    iconElementRight={<FlatButton label="編集" />}
+    iconElementLeft={<FlatButton icon={<FontAwesome name='chevron-left' size='2x' inverse='true' />} className='nav-icon' />}
+    iconElementRight={<FlatButton label='編集' labelStyle={styles.edit} />}
     className='hand-navbar'
   />
 );
