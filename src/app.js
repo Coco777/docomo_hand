991e9d19dbd42b59ae557eb9b8b203744630cf42
Koki Sato <s.4115.koki@gmail.com>
2017-08-12 19:18:21 +0900

    Use webpack and node-scss to build assets

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render((
+  <div>
+    <div className="app-header">
+      Hello HANd!
+    </div>
+  </div>
+), document.getElementById('app'));
